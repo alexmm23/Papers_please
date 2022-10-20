f8abcefa2d1995ed949baae2cbe80c541aaab7b9
Alex <a22110135@ceti.mx>
2022-10-20 08:51:44 -0500

    Menu de juego agregado y clase player

--- PapersPlease/document.h
+++ PapersPlease/document.h
@@ -24,13 +24,24 @@ public:
     virtual void ImprimirDocumento();
     friend ostream& operator <<(ostream& os, Document& document);
 };
+void Document::ImprimirDocumento() {
+    cout << "Nombre: " << nombre << endl;
+    cout << "Curp: " << curp << endl;
+    cout << "Fecha de nacimiento: " << FechadeNacimiento << endl;
+    cout << "Sexo: " << sexo << endl;
+    cout << "Firma: " << firma << endl;
+
+}
 ostream& operator << (ostream& os, Document& document) {
+
+ 
     os << "Nombre: " << document.getNombre() << endl 
-       << "Curp: " << document.getCurp() << endl 
+       << "Curp: " << document.getCurp() << endl
        << "Fecha de nacimiento: " << document.getFnac() << endl
        << "Fecha de vigencia: " << document.getFvig() << endl 
        << "Firma: " << document.getFirm() << endl 
        << "Sexo: " << document.getSexo() << endl;
+
     return os;
 }
 Document::Document(string _nombre, string _curp, string _fechaNacimiento, string _fechaVigencia, string _firma, char _sexo) {
@@ -41,12 +52,5 @@ Document::Document(string _nombre, string _curp, string _fechaNacimiento, string
     firma = _firma;
     sexo = _sexo;
 }
-void Document::ImprimirDocumento() {
-    std::cout << "Nombre: " << nombre << std::endl;
-    std::cout << "Curp: " << curp << std::endl;
-    std::cout << "Fecha de nacimiento: " << FechadeNacimiento << std::endl;
-    std::cout << "Sexo: " << sexo << std::endl;
-    std::cout << "Firma: " << firma << std::endl;
 
-}
 
